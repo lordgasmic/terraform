d66cc3402cd9ba10a436705afebd3537afecd7f8
Nick Wellman <wellman.nicholas@gmail.com>
2026-04-16 23:40:27 -0400

    attempt terraform

--- vms/main.tf
+++ vms/main.tf
@@ -1,36 +1,39 @@
 terraform {
-required_providers {
-proxmox = {
-source = "bpg/proxmox"
-version = "0.66.1"
-}
-}
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.102.0"
+    }
+  }
 }
 provider "proxmox" {
-endpoint = "https://pve.internal.lordgasmic.com/"
-api_token = "terraform-prov@pve!terraform=3d269b89-9e2f-47c9-81c5-b1f6a4f8538b"
-insecure = true
+  endpoint  = "https://pve.internal.lordgasmic.com/"
+  api_token = "terraform-prov@pve!terraform=3d269b89-9e2f-47c9-81c5-b1f6a4f8538b"
+  insecure  = false
 }
-resource "proxmox_virtual_machine" "debian_trixie" {
-name = "debian-trixie-prod"
-node_name = "pve"
-cpu { cores = 2 }
-memory { dedicated = 2048 }
-disk {
-datastore_id = "local-lvm"
-file_id = "local:iso/debian-13-generic-amd64.qcow2"
-size = 20
-}
-initialization {
-ip_config {
-ipv4 {
-address = "192.168.1.100/24"
-gateway = "192.168.1.1"
-}
-}
-user_account {
-username = "debian"
-keys = ["ssh-ed25519 AAAAC3..."]
-}
-}
+resource "proxmox_virtual_environment_vm" "test_vm" {
+  name      = "lgc-test-vm"
+  node_name = "pve"
+  cpu { cores = 2 }
+  memory { dedicated = 2048 }
+  disk {
+    datastore_id = "local"
+    file_id      = "local:iso/debian-13-generic-amd64-20260413-2447.qcow2"
+    size         = 20
+    interface    = "scsi0"
+  }
+  initialization {
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+    }
+    user_account {
+      username = "debian"
+      keys     = ["ssh-ed25519 AAAAC3..."]
+    }
+  }
+  operation_system {
+    type = "126"
+  }
 }

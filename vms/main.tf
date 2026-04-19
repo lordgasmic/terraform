2f5dcb69d1bdfd653ec03e16e00b35cc8f2569f5
Nick Wellman <wellman.nicholas@gmail.com>
2026-04-19 00:07:51 -0400

    attempt terraform

--- vms/main.tf
+++ vms/main.tf
@@ -31,9 +31,10 @@ resource "proxmox_virtual_environment_vm" "test_vm" {
   memory { dedicated = 2048 }
   disk {
     datastore_id = "datastore"
-    file_id      = "local:images/debian-13-generic-amd64-20260413-2447.qcow2"
-    size         = 20
-    interface    = "scsi0"
+    # file_id      = "local:images/debian-13-generic-amd64-20260413-2447.qcow2"
+    import_from = proxmox_virtual_environment_download_file.latest_debian_13.id
+    size      = 20
+    interface = "scsi0"
   }
   initialization {
     ip_config {
@@ -55,6 +56,15 @@ resource "random_password" "ubuntu_vm_password" {
   length           = 16
   override_special = "_%@"
   special          = true
+}
+
+resource "proxmox_virtual_environment_download_file" "latest_debian_13" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "pve"
+  url          = "https://cloud.debian.org/images/cloud/trixie/20260413-2447/debian-13-generic-amd64-20260413-2447.qcow2"
+  # need to rename the file to *.qcow2 to indicate the actual file format for import
+  file_name = "debian-13-generic-amd64.qcow2"
 }
 
 output "ubuntu_vm_password" {
